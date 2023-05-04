8a52e56d34e062f53faf193a95b662900d2c7af7
Nicholas Fasulo <fasulo.nick@gmail.com>
2023-05-04 11:03:32 -0400

    create signup component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { useState } from 'react'
+import Signup from './components/Signup'
+import { Container } from 'react-bootstrap'
 
 export default function App() {
   return (
-    <div>
-      <h1>Kanbanify</h1>
-    </div>
+    <Container className='d-flex align-items-center justify-content-center min-vh-100'>
+      <div className='w-100' style={{ maxWidth: 400 }}>
+        <Signup />
+      </div>
+    </Container>
   )
 }
