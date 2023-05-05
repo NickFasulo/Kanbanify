1c27dc39d8bc17cc5efe3dc4895c539a83bb0e37
Nicholas Fasulo <fasulo.nick@gmail.com>
2023-05-05 15:09:03 -0400

    create auth context

---
+++ src/contexts/AuthContext.js
@@ -0,0 +1,30 @@
+import { useContext, useState, useEffect } from 'react'
+import { auth } from '../firebase'
+
+const AuthContext = React.createContext()
+
+export function useAuth() {
+  return useContext(AuthContext)
+}
+
+export function AuthProdiver({ children }) {
+  const [currentUser, setCurrentUser] = useState()
+
+  function signup(email, password) {
+    return auth.createUserWithEmailAndPassword(email, password)
+  }
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      setCurrentUser(user)
+    })
+
+    return unsubscribe
+  }, [])
+
+  const value = {
+    currentUser
+  }
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+}
