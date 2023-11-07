6696582cacb0d904b02b3ec79453f95633a5fe30
Nicholas Fasulo <fasulo.nick@gmail.com>
2023-11-06 20:24:30 -0500

    start dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Card, Button } from 'react-bootstrap'
 
 export default function Dashboard() {
+  function handleLogout() {
+
+  }
+
   return (
-    <div>Dashboard</div>
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className='text-center mb-4'>Dashboard</h2>
+        </Card.Body>
+      </Card>
+      <div className='w-100 text-center mt-2'>
+        <Button variant='link' onClick={handleLogout}>
+          Log Out
+        </Button>
+      </div>
+    </>
   )
 }
